52405943e86547851aa805619f0c9d6298c2357e
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2017-03-13 14:59:37 -0400

    Doc update

--- doxygen/html/search/files_0.js
+++ doxygen/html/search/files_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['colvardeps_2eh',['colvardeps.h',['../colvardeps_8h.html',1,'']]],
   ['colvarmodule_2eh',['colvarmodule.h',['../colvarmodule_8h.html',1,'']]],
   ['colvarparse_2eh',['colvarparse.h',['../colvarparse_8h.html',1,'']]]
 ];
