79b32080c46999b37341e476f353903a74ff71e3
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2020-09-18 17:36:18 -0400

    Update doc

--- doxygen/html/search/all_a.js
+++ doxygen/html/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['jacobi_667',['jacobi',['../classcolvarmodule_1_1rotation.html#ab721e245eca92332216e9f4b44ddc212',1,'colvarmodule::rotation']]],
   ['jacobian_5fderivative_668',['Jacobian_derivative',['../classcolvar_1_1cvc.html#a302ea805321f9f0978b0dc16e1ea4581',1,'colvar::cvc']]],
   ['jd_669',['jd',['../classcolvar_1_1cvc.html#a12e8a53f1077996e42a4791e3d3074ca',1,'colvar::cvc']]]
 ];
--- doxygen/html/search/variables_9.js
+++ doxygen/html/search/variables_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['jacobi_2267',['jacobi',['../classcolvarmodule_1_1rotation.html#ab721e245eca92332216e9f4b44ddc212',1,'colvarmodule::rotation']]],
   ['jd_2268',['jd',['../classcolvar_1_1cvc.html#a12e8a53f1077996e42a4791e3d3074ca',1,'colvar::cvc']]]
 ];
