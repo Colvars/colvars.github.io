162386097e9d6aad0e9d677259af638a1070227e
CI runner <noreply@github.com>
2024-10-22 21:38:37 +0000

    Update doc for version "master"

--- doxygen/html/search/all_a.js
+++ doxygen/html/search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['jacobi_0',['jacobi',['../classcolvarmodule_1_1rotation.html#ab721e245eca92332216e9f4b44ddc212',1,'colvarmodule::rotation']]],
   ['jacobian_5fderivative_1',['Jacobian_derivative',['../classcolvar_1_1cvc.html#a302ea805321f9f0978b0dc16e1ea4581',1,'colvar::cvc']]],
-  ['jd_2',['jd',['../classcolvar_1_1cvc.html#a12e8a53f1077996e42a4791e3d3074ca',1,'colvar::cvc']]]
+  ['jd_2',['jd',['../classcolvar_1_1cvc.html#a12e8a53f1077996e42a4791e3d3074ca',1,'colvar::cvc']]],
+  ['join_5fpaths_3',['join_paths',['../classcolvarproxy__io.html#a20f8f1346c49200295bceafdb4725b97',1,'colvarproxy_io']]]
 ];
--- doxygen/html/search/functions_a.js
+++ doxygen/html/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['jacobian_5fderivative_0',['Jacobian_derivative',['../classcolvar_1_1cvc.html#a302ea805321f9f0978b0dc16e1ea4581',1,'colvar::cvc']]]
+  ['jacobian_5fderivative_0',['Jacobian_derivative',['../classcolvar_1_1cvc.html#a302ea805321f9f0978b0dc16e1ea4581',1,'colvar::cvc']]],
+  ['join_5fpaths_1',['join_paths',['../classcolvarproxy__io.html#a20f8f1346c49200295bceafdb4725b97',1,'colvarproxy_io']]]
 ];
