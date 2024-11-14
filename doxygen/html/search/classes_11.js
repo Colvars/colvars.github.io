f6193c0039f111ec457a9d4de7edb50e976504e2
CI runner <noreply@github.com>
2024-11-14 16:09:18 +0000

    Update doc for version "master"

--- doxygen/html/search/classes_11.js
+++ doxygen/html/search/classes_11.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['tilt_0',['tilt',['../classcolvar_1_1tilt.html',1,'colvar']]],
-  ['traj_5fline_1',['traj_line',['../structcolvarbias__opes_1_1traj__line.html',1,'colvarbias_opes']]]
+  ['torchann_1',['torchANN',['../classcolvar_1_1torchANN.html',1,'colvar']]],
+  ['traj_5fline_2',['traj_line',['../structcolvarbias__opes_1_1traj__line.html',1,'colvarbias_opes']]]
 ];
